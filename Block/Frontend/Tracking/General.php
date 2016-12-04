02d539fb136751da036708079262330d330facba
bnayal <bnaya@remarkety.com>
2016-12-04 13:52:13 +0200

    bugfix - cache product page

--- Block/Frontend/Tracking/General.php
+++ Block/Frontend/Tracking/General.php
@@ -1,5 +1,14 @@
 <?php
 namespace Remarkety\Mgconnector\Block\Frontend\Tracking;
-class General extends Base {
 
+use Magento\Store\Model\StoreManager;
+use Magento\Framework\View\Element\Template\Context;
+use \Remarkety\Mgconnector\Model\Webtracking;
+use Magento\Customer\Model\Session;
+
+class General extends Base {
+    public function __construct(Context $context, array $data = [], StoreManager $sManager, Webtracking $webtracking, Session $session){
+        parent::__construct($context, $data, $sManager, $webtracking, $session);
+        $this->_isScopePrivate = true;
+    }
 }
